9ffc1d463c400874aee117d66b16f41ce5536d9b
In A Jung <injung@c3r3p6.hive.fi>
2024-01-19 12:55:18 +0200

    0119 1

--- c04/ex03/ft_atoi.c
+++ c04/ex03/ft_atoi.c
@@ -5,82 +5,53 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: injung <injung@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/16 11:58:53 by injung            #+#    #+#             */
-/*   Updated: 2024/01/18 14:10:08 by injung           ###   ########.fr       */
+/*   Created: 2024/01/19 11:05:02 by injung            #+#    #+#             */
+/*   Updated: 2024/01/19 11:17:57 by injung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	is_white_space(char c)
 {
-	if (c == ' ' | c == '\t' | c == '\n')
+	if ((c == '\t') || (c == '\n') || (c == '\v'))
 		return (1);
-	if (c == '\f' | c == '\v' | c == '\r')
+	if ((c == '\f') || (c == '\r') || (c == ' '))
 		return (1);
-	else
-		return (0);
-}
-
-int	find_sign(char **str)
-{
-	int		count;
-
-	count = 0;
-	while (**str && ((**str < '0') || (**str > '9')))
-	{
-		if (**str == '-')
-			count++;
-		else if (**str != '+')
-			return (0);
-		++(*str);
-	}
-	if (count % 2)
-		return (-1);
-	else
-		return (1);
-}
-
-int	find_num(long int pow, char *str)
-{
-	int		res;
-
-	res = 0;
-	while (pow >= 10)
-	{
-		pow /= 10;
-		res += ((*str - '0') * pow);
-		str++;
-	}
-	return (res);
+	return (0);
 }
 
 int	ft_atoi(char *str)
 {
-	int			len;
-	int			sign;
-	int			res;
-	long int	pow;
+	int		res;
+	int		sign;
 
-	while (is_white_space(*str))
+	res = 0;
+	while (*str && is_white_space(*str))
 		str++;
-	sign = find_sign(&str);
-	pow = 1;
-	len = 0;
-	while (str[len] && ((str[len] >= '0') && (str[len] <= '9')))
+	sign = 0;
+	while (*str && ((*str == '+') || (*str == '-')))
 	{
-		len++;
-		pow *= 10;
+		if (*str == '-')
+			sign++;
+		str++;
 	}
-	res = find_num(pow, str);
-	return (sign * res);
+	while (*str && (*str >= '0') && (*str <= '9'))
+	{
+		res = res * 10 + (*str - '0');
+		str++;
+	}
+	if (sign % 2)
+		return (res * (-1));
+	else
+		return (res);
 }
 
 /*
 #include <stdio.h>
 int	main(void)
 {
-	char s1[] = " \t--+--+1234ab567";
+	char s1[] = " \t---+--+1234ab567";
 	printf("%s: %d\n", s1, ft_atoi(s1));
-	printf("%s: %d\n", "1325632167", ft_atoi("1325632167"));
-	printf("%s: %d\n", "-2147483648", ft_atoi("-2147483648"));
+	printf("%s: %d\n", "--+- +1325632167", ft_atoi("--+= +1325632167"));
+	printf("%s: %d\n", " 2147483647", ft_atoi(" 2147483647"));
 }
 */
--- c04/ex05/ft_atoi_base.c
+++ c04/ex05/ft_atoi_base.c
@@ -6,38 +6,17 @@
 /*   By: injung <injung@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/16 16:50:02 by injung            #+#    #+#             */
-/*   Updated: 2024/01/18 17:24:07 by injung           ###   ########.fr       */
+/*   Updated: 2024/01/19 11:44:18 by injung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	find_num(char c, char *base);
-
-int	find_sign(char **str, char *base)
+int	is_white_space(char c)
 {
-	int		count;
-
-	while (**str)
-	{
-		if (**str == ' ' | **str == '\t' | **str == '\n')
-			++(*str);
-		else if (**str == '\f' | **str == '\v' | **str == '\r')
-			++(*str);
-		else
-			break ;
-	}
-	count = 0;
-	while (**str && (find_num(**str, base) == -1))
-	{
-		if (**str == '-')
-			count++;
-		else if (**str != '+')
-			return (0);
-		++(*str);
-	}
-	if (count % 2)
-		return (-1);
-	else
+	if ((c == '\t') || (c == '\n') || (c == '\v'))
 		return (1);
+	if ((c == '\f') || (c == '\r') || (c == ' '))
+		return (1);
+	return (0);
 }
 
 int	check_conditions(char *base)
@@ -50,10 +29,12 @@ int	check_conditions(char *base)
 	while (base[size])
 	{
 		c = base[size];
-		if ((c == '+') || (c == '-') || (c == ' '))
+		if (is_white_space(c))
 			return (0);
-		i = size + 1;
-		while (base[i])
+		if ((c == '+') || (c == '-'))
+			return (0);
+		i = 0;
+		while (base[i] && (i < size))
 		{
 			if (c == base[i])
 				return (0);
@@ -62,22 +43,6 @@ int	check_conditions(char *base)
 		size++;
 	}
 	return (size);
-}
-
-long int	powing(char *str, long int bsize, char *base)
-{
-	int				i;
-	long int		pow;
-
-	pow = 1;
-	i = 0;
-	while (str[i] && (find_num(str[i], base) != -1))
-	{
-		i++;
-		pow *= bsize;
-	}
-	pow /= bsize;
-	return (pow);
 }
 
 int	find_num(char c, char *base)
@@ -98,28 +63,29 @@ int	ft_atoi_base(char *str, char *base)
 {
 	int				bsize;
 	int				res;
-	long int		pow;
 	int				sign;
-	int				i;
 
+	res = 0;
 	bsize = check_conditions(base);
 	if (bsize <= 1)
 		return (0);
-	sign = find_sign(&str, base);
-	if (sign == 0)
-		return (0);
-	pow = powing(str, bsize, base);
-	res = 0;
-	while (*str)
-	{
-		i = find_num(*str, base);
-		if (i == -1)
-			break ;
-		res += i * pow;
+	while (*str && is_white_space(*str))
 		str++;
-		pow /= bsize;
+	while (*str && ((*str == '+') || (*str == '-')))
+	{
+		if (*str == '-')
+			sign++;
+		str++;
 	}
-	return (res * sign);
+	while (find_num(*str, base) != -1)
+	{
+		res = res * bsize + find_num(*str, base);
+		str++;
+	}
+	if (sign % 2)
+		return (res * (-1));
+	else
+		return (res);
 }
 
 /*
