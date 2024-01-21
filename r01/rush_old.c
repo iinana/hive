b75de383e3cad4aa9f711096d1eaa150168dfe26
In A Jung <injung@c3r3p1.hive.fi>
2024-01-21 20:23:32 +0200

    rush01 20:23

--- r01/rush_old.c
+++ r01/rush_old.c
@@ -6,7 +6,7 @@
 /*   By: injung <injung@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 10:25:43 by injung            #+#    #+#             */
-/*   Updated: 2024/01/21 20:22:14 by injung           ###   ########.fr       */
+/*   Updated: 2024/01/21 18:42:19 by injung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -81,7 +81,7 @@ int	check_col(int *res, int cur_col, int u, int d)
 	return (1);
 }
 
-int check_rowl(int i, int cur_col, int cur_row, int *res, int l)
+int check(int i, int cur_col, int cur_row, int *res, int l, int r)
 {
 	int		j;
 	int		max;
@@ -107,28 +107,18 @@ int check_rowl(int i, int cur_col, int cur_row, int *res, int l)
 		count++;
 	if (count > l)
 		return (0);
-	return (1);
-}
-
-int check_rowr(int cur_row, int *res, int r)
-{
-	int		i;
-	int		max;
-	int		count;
-
-	i = 3;
-	max = 0;
-	count = 0;
-	while (i >= 0)
+	max = i;
+	count = 1;
+	while (j > 0)
 	{
-		if (res[cur_row * 4 + i] > max)
+		j--;
+		if (res[cur_row * 4 + j] > max)
 		{
-			max = res[cur_row * 4 + i];
+			max = res[cur_row * 4 + j];
 			count++;
 		}
-		i--;
 	}
-	if (count != r)
+	if (count > r)
 		return (0);
 	return (1);
 }
@@ -150,18 +140,14 @@ int	put_row(int cur_col, int cur_row, int *row, int *col,  int **res)
 	}
 	if (cur_col >= 4)
 	{
-		if (check_rowr(cur_row, *res, row[cur_row + 4]))
-		{
-			if (put_row(0, cur_row + 1, row, col, res))
-				return (1);
-			return (0);
-		}
+		if (put_row(0, cur_row + 1, row, col, res))
+			return (1);
 		return (0);
 	}
 	i = 1;
 	while (i <= 4)
 	{
-		if (check_rowl(i, cur_col, cur_row, *res, row[cur_row]))
+		if (check(i, cur_col, cur_row, *res, row[cur_row], row[cur_row + 4]))
 		{
 			(*res)[cur_row * 4 + cur_col] = i;
 			if (put_row(cur_col + 1, cur_row, row, col, res))
@@ -172,31 +158,12 @@ int	put_row(int cur_col, int cur_row, int *row, int *col,  int **res)
 	return (0);
 }
 
-void	print_res(int wid, int hei, int *res)
-{
-	int		w;
-	int		h;
-	char	c;
-
-	h = 0;
-	while (h < hei)
-	{
-		w = 0;
-		while (w < wid)
-		{
-			c = res[h * wid + w] + '0';
-			write(1, &c, 1);
-			w++;
-		}
-		write(1, "\n", 1);
-		h++;
-	}
-}
-
 int	main(int argc, char **argv)
 {
 	int		**input;
 	int		*res;
+	int		i;
+	char	c;
 	
 	input = (int **)malloc(64);
 	res = (int *)malloc(64);
@@ -207,11 +174,28 @@ int	main(int argc, char **argv)
 	}	
 	input[0] = make_input(argv + 1);
 	input[1] = make_input(argv + 1);
-	if (input[0][0] && input[1][0] && put_row(0, 0, input[1], input[0], &res))
-		print_res(4, 4, res);
+	if (!(input[0][0] && input[1][0]))
+	{
+		write(1, "Error\n", 6);
+		return (0);
+	}
+	if (put_row(0, 0, input[0], input[1], &res))
+	{
+		i = 0;
+		while (i < 16)
+		{
+			c = res[i] + '0';
+			write(1, &c, 1);
+			if (i % 4 == 3)
+				write(1, "\n", 1);
+			i++;
+		}
+	}
 	else
 	{
-		write(1, "2Error\n", 6);
+		write(1, "Error\n", 6);
 		return (0);
 	}
 }
+
+
--- r01/test.c
+++ r01/test.c
@@ -6,13 +6,15 @@
 /*   By: injung <injung@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 10:25:43 by injung            #+#    #+#             */
-/*   Updated: 2024/01/21 18:38:47 by injung           ###   ########.fr       */
+/*   Updated: 2024/01/21 19:51:27 by injung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdlib.h>
 #include <unistd.h>
 #include <stdio.h>
+
+void	print_res(int wid, int hei, int *res);
 
 int	*make_input(char **input)
 {
@@ -104,10 +106,12 @@ int check(int i, int cur_col, int cur_row, int *res, int l, int r)
 		}
 		j++;
 	}
+	if (cur_col == 2 && cur_row == 1 && i == 1) printf("pass 1 | ");
 	if (i > max)
 		count++;
 	if (count > l)
 		return (0);
+	if (cur_col == 2 && cur_row == 1 && i == 1) printf("pass 2 with %d | ", count);
 	max = i;
 	count = 1;
 	while (j > 0)
@@ -121,23 +125,20 @@ int check(int i, int cur_col, int cur_row, int *res, int l, int r)
 	}
 	if (count > r)
 		return (0);
+	if (cur_col == 2 && cur_row == 1 && i == 1) printf("pass 3 with %d | ", count);
 	return (1);
 }
 
 int	put_row(int cur_col, int cur_row, int *row, int *col,  int **res)
 {
 	int		i;
-	char	c;
 
 	if ((cur_row == 3) && (cur_col >= 4))
 	{
+		print_res(4, 4, *res);
 		i = 0;
 		while (i < 4)
 		{
-			c = (*res)[i] + '0';
-			write(1, &c, 1);
-			if (i % 4 == 3)
-				write(1, "\n", 1);
 			if (check_col(*res, i, col[i], col[i + 4]) == 0)
 				return (0);
 			i++;
@@ -146,6 +147,7 @@ int	put_row(int cur_col, int cur_row, int *row, int *col,  int **res)
 	}
 	if (cur_col >= 4)
 	{
+		printf("row%d col%d | %d %d %d %d\n", cur_row, cur_col, (*res)[cur_row * 4], (*res)[cur_row * 4 + 1], (*res)[cur_row * 4 + 2], (*res)[cur_row * 4 + 3]);
 		if (put_row(0, cur_row + 1, row, col, res))
 			return (1);
 		return (0);
@@ -155,7 +157,7 @@ int	put_row(int cur_col, int cur_row, int *row, int *col,  int **res)
 	{
 		if (check(i, cur_col, cur_row, *res, row[cur_row], row[cur_row + 4]))
 		{
-			printf("cur_col=%d, cur_row=%d\n", cur_col, cur_row);
+			if (cur_row >= 1) printf("col=%d, i=%d\n", cur_col, i);
 			(*res)[cur_row * 4 + cur_col] = i;
 			if (put_row(cur_col + 1, cur_row, row, col, res))
 				return (1);
@@ -165,12 +167,31 @@ int	put_row(int cur_col, int cur_row, int *row, int *col,  int **res)
 	return (0);
 }
 
+void	print_res(int wid, int hei, int *res)
+{
+	int		w;
+	int		h;
+	char	c;
+
+	h = 0;
+	while (h < hei)
+	{
+		w = 0;
+		while (w < wid)
+		{
+			c = res[h * wid + w] + '0';
+			write(1, &c, 1);
+			w++;
+		}
+		write(1, "\n", 1);
+		h++;
+	}
+}
+
 int	main(int argc, char **argv)
 {
 	int		**input;
 	int		*res;
-	int		i;
-	char	c;
 	
 	input = (int **)malloc(64);
 	res = (int *)malloc(64);
@@ -181,29 +202,14 @@ int	main(int argc, char **argv)
 	}	
 	input[0] = make_input(argv + 1);
 	input[1] = make_input(argv + 1);
-	if (!(input[0][0] && input[1][0]))
+	if (input[0][0] && input[1][0])
+	{
+		put_row(0, 0, input[1], input[0], &res);
+		print_res(4, 4, res);
+	}
+	else
 	{
 		write(1, "2Error\n", 6);
 		return (0);
 	}
-	i = put_row(0, 0, input[0], input[1], &res);
-	if (1)
-	{
-		i = 0;
-		while (i < 16)
-		{
-			c = res[i] + '0';
-			write(1, &c, 1);
-			if (i % 4 == 3)
-				write(1, "\n", 1);
-			i++;
-		}
-	}
-	else
-	{
-		write(1, "3Error\n", 6);
-		return (0);
-	}
 }
-
-
