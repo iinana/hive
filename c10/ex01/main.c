ab9f79f82b878f7a74c0b16a828250e0a47711c7
In A Jung <injung@c3r3p6.hive.fi>
2024-01-25 21:37:36 +0200

    0125

--- c10/ex01/main.c
+++ c10/ex01/main.c
@@ -5,43 +5,63 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: injung <injung@student.hive.fi>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/01/25 15:31:48 by injung            #+#    #+#             */
-/*   Updated: 2024/01/25 16:56:25 by injung           ###   ########.fr       */
+/*   Created: 2024/01/25 16:55:35 by injung            #+#    #+#             */
+/*   Updated: 2024/01/25 21:37:14 by injung           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <fcntl.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <string.h>
 
 void	ft_putstr(char *str)
 {
 	while (*str)
-	{
-		write(1, str, 1);
-		str++;
-	}
+		write(1, str++, 1);
 }
 
-int	main(int argc, char **argv)
+int	put_error(char *path)
+{
+	if (errno == 0)
+		return (0);
+	ft_putstr("./ft_cat: ");
+	ft_putstr(path);
+	ft_putstr(": ");
+	ft_putstr(strerror(errno));
+	ft_putstr("\n");
+	return (1);
+
+}
+
+int	print_single_file(char *file)
 {
 	int				infile;
 	unsigned char	buf;
 
-	if (argc != 2)
-	{
-		if (argc < 2)
-			ft_putstr("File name missing.\n");
-		else
-			ft_putstr("Too many arguments.\n");
-		return (0);
-	}
-	infile = open(argv[1], O_RDONLY);
+	infile = open(file, O_RDONLY);
 	if (infile < 0)
-	{
-		ft_putstr("Cannot read file.\n");
 		return (0);
-	}
 	while (read(infile, &buf, 1) > 0)
 		write(1, &buf, 1);
 	close(infile);
+	return (1);
+}
+
+int	main(int argc, char **argv)
+{
+	int		i;
+
+	if (argc < 2)
+		put_error(0);
+	i = 1;
+	while (i < argc)
+	{
+		if (print_single_file(argv[i]) == 0)
+		{
+			if (put_error(argv[i]))
+				return (0);
+		}
+		i++;
+	}
 }
